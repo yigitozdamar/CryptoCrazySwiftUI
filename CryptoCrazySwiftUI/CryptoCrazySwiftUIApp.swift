579d2a6abd97d324c9fdcaa70f439f682365d092
yigit <yigitozdamar@hotmail.com>
2022-08-23 14:06:26 +0300

    Initial Commit

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Yigit Ozdamar on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
